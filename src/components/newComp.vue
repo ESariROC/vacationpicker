9881834a90d6cc3aaed2d614a84c135df7b28a6a
ESariROC <94495713+ESariROC@users.noreply.github.com>
2022-05-16 11:28:36 +0200

    Praktijkoefeningen 1 en 2

---
+++ src/components/newComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Pick your next vacation</h1>
+  <ul class="list-group">
+    <li class="list-group-item"
+      v-for="country in countryData.countries"
+      v-bind:key="country.id">
+      {{country.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+
+import countryData from "@/data/countryData";
+export default {
+  data(){
+    return {
+      countryData,
+      name: "randomName"
+    }
+  }
+}
+
+</script>
